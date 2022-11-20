12efb4297125d52eb7bf9f79d724dd9007a12e73
bhubaneshwary <bhubaneshwar.yadav@smaitic.com>
2022-11-20 18:40:25 +0545

    add(): Google mail

---
+++ Server/src/utils/nodeMailer.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+const send = async ({ from, to, subject, text, html }) => {
+  try {
+    const res = await transporter.sendMail({
+      from: from,
+      to: to,
+      subject: subject,
+      text: text,
+      html: html,
+    });
+    if (res?.info?.response.includes("ok")) return true;
+    throw new Error("Error occur sending mail");
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = { send };
