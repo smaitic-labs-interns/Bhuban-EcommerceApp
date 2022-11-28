f016e97d31dd1f0f6dcd0496361901c1ed7e2a60
bhubaneshwary <bhubaneshwar.yadav@smaitic.com>
2022-11-28 21:08:11 +0545

    fix(): admin route enabled

--- client/src/routes/AdminRoute.js
+++ client/src/routes/AdminRoute.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-export default function AdminRoute({ children, isAuthenticated }) {
-  const userRole = "admin";
-  //   return isAuthenticated === true ? children : <Navigate to="/login" />;
-  return children;
+export default function AdminRoute({ children, role }) {
+  const roles = ["superadmin", "admin"];
+  const login = useSelector((state) => state.login);
+
+  return login.isLogined && roles.includes(login.role) ? (
+    children
+  ) : (
+    <Navigate to="/" />
+  );
 }
