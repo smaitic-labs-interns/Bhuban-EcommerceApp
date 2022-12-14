fdf86f3f868b0731d944e2f3ded6e118a0a6c560
bhubaneshwary <bhubaneshwar.yadav@smaitic.com>
2022-12-14 22:51:44 +0545

    wip(): webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,24 +1,54 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ThemesGeneratorPlugin = require("themes-switch/ThemesGeneratorPlugin");
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: "development",
+  stats: {
+    children: true,
+  },
   entry: path.join(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "my-first-webpack.bundle.js",
+    filename: "[name].[hash:8].js",
+    sourceMapFilename: "[name].[hash:8].map",
+    chunkFilename: "[id].[hash:8].js",
   },
   module: {
     rules: [
+      // {
+      //   test: /\.?js|.json$/,
+      //   exclude: [/node_modules/],
+      //   use: {
+      //     loader: "babel-loader",
+      //     options: {
+      //       presets: ["@babel/preset-env", "@babel/preset-react"],
+      //     },
+      //   },
+      // },
+      // {
+      //   "no-restricted-imports": [
+      //     "error",
+      //     {
+      //       patterns: ["@material-ui/*/*/*"],
+      //     },
+      //   ],
+      // },
       {
-        test: /\.?js|.json$/,
-        exclude: [/node_modules/],
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+        test: /\.html$/i,
+        use: [
+          {
+            loader: "html-loader",
+            options: { interpolate: true },
           },
-        },
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.json$/,
@@ -28,11 +58,18 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader", "css-to-mui-loader"],
+        use: [
+          // MiniCssExtractPlugin.loader,
+          "style-loader",
+          { loader: "css-loader", options: { esModule: false } },
+          // "css-to-mui-loader",
+          // "postcss-loader",
+        ],
       },
       {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: ["file-loader"],
+        test: /\.(png|jp(e*)g|svg|gif|webp)$/,
+        use: ["file-loader", "image-webpack-loader"],
+        exclude: /node_modules/,
       },
       {
         test: /\.svg$/,
@@ -41,9 +78,31 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.ProgressPlugin(),
+    // new ThemesGeneratorPlugin({
+    //   srcDir: "src",
+    //   // themesDir: "src/utils",
+    //   // outputDir: "dist/static/css",
+    //   defaultStyleName: "default.less",
+    // }),
+    // new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.js"),
     }),
+    // new webpack.ProgressPlugin(),
+    // new webpack.DefinePlugin({
+    //   PRODUCTION: JSON.stringify(true),
+    // }),
+    // new webpack.LoaderOptionsPlugin({
+    //   minimize: true,
+    // }),
+    // new webpack.ProvidePlugin({
+    //   L: "leaflet",
+    // }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+  },
 };
