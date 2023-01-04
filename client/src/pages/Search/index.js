ae1aec6a6283c61f8738e2a3bf69ff0741174fcf
bhubaneshwary <bhubaneshwar.yadav@smaitic.com>
2023-01-04 23:01:49 +0545

    fix(): routes

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from "react";
-import { TextField, Autocomplete, Typography } from "@mui/material";
-import SearchList from "./Components/SearchList";
+import React, { useEffect, useState } from 'react';
+import { TextField, Autocomplete, Typography } from '@mui/material';
+import SearchList from './Components/SearchList';
 import {
   SearchWrapper,
   SearchBoxWrapper,
   SearchInputWrapper,
   SearchButtonWrapper,
   SearchButton,
-} from "./Styles/searchStyle";
-import { Search } from "@mui/icons-material";
-import { search_product } from "../../redux/actions/productActions";
-import { useSelector, useDispatch } from "react-redux";
+} from './Styles/searchStyle';
+import { Search } from '@mui/icons-material';
+import { search_product } from '../../redux/actions/productActions';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function SearchPage() {
   const products = useSelector((state) => state.searchProduct);
   const dispatch = useDispatch();
 
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState('');
   const [allProduct, setAllProduct] = useState([]);
   const [productName, setProductsName] = useState([]);
   const [callSearch, setCallSearch] = useState(false);
 
   const handleChange = (key) => {
-    if (key && key !== "") {
-      dispatch(search_product({ keyword: key, action: "search" }));
+    if (key && key !== '') {
+      dispatch(search_product({ keyword: key, action: 'search' }));
     } else {
-      dispatch(search_product({ keyword: keyword, action: "clean" }));
+      dispatch(search_product({ keyword: keyword, action: 'clean' }));
     }
   };
 
   const handleSearch = () => {
     // setCallSearch(true);
-    dispatch(search_product({ keyword: keyword, action: "clean" }));
-    if (keyword && keyword !== "") {
+    dispatch(search_product({ keyword: keyword, action: 'clean' }));
+    if (keyword && keyword !== '') {
       // console.log("KeyWord: ", keyword);
-      dispatch(search_product({ keyword: keyword, action: "search" }));
+      dispatch(search_product({ keyword: keyword, action: 'search' }));
     }
   };
 
   useEffect(() => {
-    if (products.status === "success") {
+    if (products.status === 'success') {
       // let labels = [];
       // for (var prdct of products.products) {
       //   labels.push(prdct.model);
@@ -63,13 +63,13 @@ export default function SearchPage() {
           <Autocomplete
             onChange={(event, value) => setKeyword(value)}
             disablePortal
-            id="combo-box-demo"
+            id='combo-box-demo'
             options={productName}
             sx={{ width: 300 }}
             renderInput={(params) => (
               <TextField
                 {...params}
-                label="Enter Search Keyword"
+                label='Enter Search Keyword'
                 onChange={(e) => handleChange(e.target.value)}
               />
             )}
@@ -87,7 +87,7 @@ export default function SearchPage() {
       ) : callSearch === true ? (
         <Typography>NO Product Found</Typography>
       ) : (
-        ""
+        ''
       )}
     </SearchWrapper>
   );
