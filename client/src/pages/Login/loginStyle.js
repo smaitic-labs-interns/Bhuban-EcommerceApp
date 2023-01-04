e6c099b48471426b80f4adc2d334ece3d02dc142
bhubaneshwary <bhubaneshwar.yadav@smaitic.com>
2023-01-04 22:32:00 +0545

    fix(style): login style

--- client/src/pages/Login/loginStyle.js
+++ client/src/pages/Login/loginStyle.js
@@ -1,27 +1,33 @@
 // import theme from "../../../utils/theme";
-import { Box, styled, Typography } from "@mui/material";
+import { Box, styled, Typography } from '@mui/material';
 
 export const LoginWrapper = styled(Box)(() => ({
-  display: "flex",
-  justifyContent: "center",
+  display: 'flex',
+  justifyContent: 'center',
 }));
 
 export const LoginContainer = styled(Box)(() => ({
-  maxWidth: "40%",
-  marginTop: "50px",
+  maxWidth: '40%',
+  marginTop: '50px',
 }));
 
 export const HeaderWrapper = styled(Box)(() => ({}));
 export const TitleWrapper = styled(Typography)(() => ({}));
 export const FormWrapper = styled(Box)(() => ({}));
+export const FormInputWrapper = styled(Box)(() => ({
+  padding: '1rem 0',
+  display: 'flex',
+  gap: '1rem',
+  alignItems: 'center',
+}));
 export const ForgotPwdWrapper = styled(Box)(() => ({
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 }));
 
 export const CreateAccWrapper = styled(Box)(() => ({
-  padding: "1rem 0",
-  display: "flex",
-  justifyContent: "flex-end",
+  padding: '1rem 0',
+  display: 'flex',
+  justifyContent: 'flex-end',
 }));
